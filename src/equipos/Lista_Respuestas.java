46186883261eb6cd39699547d6d43d451902c918
MauroC97 <42740061+MauroC97@users.noreply.github.com>
2018-10-17 19:07:16 -0300

    procesamiento de archivos

--- src/equipos/Lista_Respuestas.java
+++ src/equipos/Lista_Respuestas.java
@@ -1,6 +1,13 @@
 package equipos;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Lista_Respuestas {
+	int preguntas,colaboradores;
 	String [] respuestas;
 	String input,output;
 	Equipo max_afinidad;
@@ -8,17 +15,30 @@ public class Lista_Respuestas {
 	public Lista_Respuestas(String input, String output) {
 		this.input = input;
 		this.output = output;
+		max_afinidad = new Equipo("a",0);
 	}
 	
-	public void obtener_respuestas() {
+	public void obtener_respuestas() throws IOException {
+		Scanner sc = new Scanner(new File(input));
+		int i;
+		this.preguntas=sc.nextInt();
+		this.colaboradores=sc.nextInt();
+		this.respuestas = new String [this.colaboradores];
+		for (i=0;i<this.colaboradores;i++)
+			this.respuestas[i]=sc.nextLine();
 		
+		sc.close();
 	}
 	
 	public void encontrar_equipo() {
 		
+		
 	}
 	
-	public void generar_arch_salida() {
-		
+	public void generar_arch_salida() throws IOException {
+		PrintWriter pw = new PrintWriter(new FileWriter(output));
+		pw.println(max_afinidad.afinidad);
+		pw.println(max_afinidad.resp_en_comun);
+		pw.close();
 	}
 }
